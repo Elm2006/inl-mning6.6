ab3d3da0b3409b66d38d1b650655926e152a7a51
Elm2006 <isel06002@utb.vaxjo.se>
2024-02-19 14:05:47 +0100

    Add project files.

---
+++ inlämning6.6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string x = Console.ReadLine();
+        VartAnnatOrd(x);
+    }
+    static void VartAnnatOrd(string x)
+    {
+        string[] y = x.Split(' ');
+        for (int i = 0; i < y.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                y[i] = y[i].ToUpper();
+                Console.Write(y[i]+" ");
+            }
+            else
+            {
+                y[i] = y[i].ToLower();
+                Console.Write(y[i] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+}
